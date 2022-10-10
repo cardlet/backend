55b0d12bec5eb49cd3ab0f5d125483b3cda575d0
Bnyro <bnyro@tutanota.com>
2022-10-10 17:54:45 +0200

    improved CORS

--- api/router.go
+++ api/router.go
@@ -20,8 +20,8 @@ func CreateRouter() {
 		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
 		AllowedOrigins:   []string{"https://*", "http://*"},
 		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
-		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type"},
 		ExposedHeaders:   []string{"Link"},
 		AllowCredentials: false,
 		MaxAge:           300, // Maximum value not ignored by any of major browsers
@@ -73,5 +73,5 @@ func CardsRouter(r chi.Router) {
 	r.Post("/create", createCard)
 	r.Get("/user/{id}", getCardsByDeskId)
 	r.Get("/{id}", getCardsByUser)
-	// r.Delete("/delete", deleteCard)
+	r.Delete("/delete/{id}", deleteCard)
 }
